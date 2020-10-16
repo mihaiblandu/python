b053212dd0141e852a474e54bd52a16f1c509a7f
mihai blandu <mihaiblandu@icloud.com>
2020-10-16 09:14:23 +0300

    po

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django import utils
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True,null=True,default="this is cool")
-    price = models.DecimalField(decimal_places=2,max_digits=10000)
+    price = models.DecimalField(decimal_places=2,max_digits=10)
     summary = models.TextField(blank=False,null=False,default="this is summary")
     feature = models.BooleanField(default=False)
     date = models.DateField(default=utils.timezone.now)
